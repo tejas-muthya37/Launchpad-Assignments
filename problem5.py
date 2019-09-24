6b42a03b9e5d9094659f6167e968a0e6b8d84122
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2019-09-24 19:32:08 +0530

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,4 @@
+n = input().split()
+while i<len(n):
+    print(n[len(n)-i-1], end = " ")
+    i=i+1
---
+++ problem6.py
@@ -0,0 +1,8 @@
+d = {:}
+i = 0
+while (i<3):
+    name = input('enter the name : ')
+    usn = input('enter the usn : ')
+    i=i+1
+    dict[usn] = name
+print(dict)
