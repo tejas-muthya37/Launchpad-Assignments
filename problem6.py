87e53db5fd7504aac06ddcbc0a45c7e6fa6d19b0
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2019-09-25 17:45:45 +0530

    Add files via upload

--- problem6.py
+++ problem6.py
@@ -1,8 +1,9 @@
-d = {:}
+d = {}
 i = 0
 while (i<3):
     name = input('enter the name : ')
     usn = input('enter the usn : ')
     i=i+1
-    dict[usn] = name
-print(dict)
+    d[usn] = name
+print(d)
+print("\n")
